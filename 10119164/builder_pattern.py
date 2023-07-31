c473f8ca3ae4e0d743ff699114000f9a93a553ca
siddiq sanjaya bakti <44673223+sidiqsanjaya@users.noreply.github.com>
2023-07-31 20:24:48 +0700

    Update builder_pattern.py
    menambahkan komentar agar lebih jelas

--- 10119164/builder_pattern.py
+++ 10119164/builder_pattern.py
@@ -1,5 +1,8 @@
 # pola creational
 
+# Penerapan Pola Desain "Builder" (Pola Creational)
+
+# Kelas SpeedTest bertanggung jawab untuk mengatur dan menjalankan serangkaian tes.
 class SpeedTest:
     def __init__(self):
         self._tests = []
@@ -11,12 +14,15 @@ class SpeedTest:
         for test in self._tests:
             test.run()
 
+# Kelas TestResult digunakan untuk menyimpan hasil dari setiap tes yang dijalankan.
 class TestResult:
     def __init__(self):
         self.result = {}
 
     def add_result(self, test_type, value):
         self.result[test_type] = value
+
+# Kelas-kelas berikut merupakan tes-tipe yang akan dijalankan dalam SpeedTest.
 
 class KecepatanTest:
     def run(self):
@@ -33,6 +39,8 @@ class JitterTest:
 class LatencyTest:
     def run(self):
         print("Mengukur latency...")
+
+# Kelas SpeedTestBuilder bertanggung jawab untuk membangun serangkaian tes sesuai dengan kebutuhan.
 
 class SpeedTestBuilder:
     def __init__(self):
@@ -53,12 +61,19 @@ class SpeedTestBuilder:
     def get_speed_test(self):
         return self._speed_test
 
-# penggunaan
+# Penggunaan Pola Desain "Builder" di atas:
+
+# Membuat objek SpeedTestBuilder untuk membangun serangkaian tes.
 builder = SpeedTestBuilder()
+
+# Memilih tes-tes yang akan dijalankan dan menambahkannya ke dalam SpeedTest.
 builder.build_kecepatan_test()
 builder.build_ping_test()
 builder.build_jitter_test()
 builder.build_latency_test()
 
+# Mendapatkan objek SpeedTest yang sudah dibangun.
 speed_test = builder.get_speed_test()
+
+# Menjalankan serangkaian tes yang sudah dibuat.
 speed_test.run_tests()
