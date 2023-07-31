57f868683f82d3c487d69e67006fcebedcddb5b4
siddiq sanjaya bakti <44673223+sidiqsanjaya@users.noreply.github.com>
2023-07-31 20:30:08 +0700

    Update facade_pattern.py
    menambahkan komentar agar lebih jelas

--- 10119164/facade_pattern.py
+++ 10119164/facade_pattern.py
@@ -1,5 +1,9 @@
 # pola struktural
 
+# Penerapan Pola Desain "Facade" (Pola Struktural)
+
+# Kelas-kelas berikut merepresentasikan tes-tipe yang berbeda dan memiliki fungsi run_test() untuk menjalankan tes.
+
 class KecepatanTest:
     def run_test(self):
         print("Mengukur kecepatan internet...")
@@ -16,19 +20,27 @@ class LatencyTest:
     def run_test(self):
         print("Mengukur latency...")
 
+# Kelas SpeedTesterFacade bertindak sebagai facade (antarmuka sederhana) yang menyediakan akses terkoordinasi untuk mengakses tes-tipe.
+
 class SpeedTesterFacade:
     def __init__(self):
+        # Inisialisasi objek-objek tes yang akan digunakan dalam facade.
         self.kecepatan_tester = KecepatanTest()
         self.ping_tester = PingTest()
         self.jitter_tester = JitterTest()
         self.latency_tester = LatencyTest()
 
     def test_speed(self):
+        # Memanggil fungsi run_test() untuk menjalankan setiap tes-tipe.
         self.kecepatan_tester.run_test()
         self.ping_tester.run_test()
         self.jitter_tester.run_test()
         self.latency_tester.run_test()
 
-# penggunaan
+# Penggunaan Pola Desain "Facade" di atas:
+
+# Membuat objek SpeedTesterFacade sebagai antarmuka sederhana untuk mengakses tes-tipe.
 speed_tester = SpeedTesterFacade()
+
+# Memanggil fungsi test_speed() pada SpeedTesterFacade untuk menjalankan semua tes-tipe dengan satu perintah.
 speed_tester.test_speed()
