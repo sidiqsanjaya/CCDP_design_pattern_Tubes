cc4f6f142e511d0f5d147a04d01900951b3a78ef
siddiq sanjaya bakti <44673223+sidiqsanjaya@users.noreply.github.com>
2023-07-31 20:09:52 +0700

    Update prototype_pattern.py
    menambahkan komentar agar lebih jelas

--- 10119167/prototype_pattern.py
+++ 10119167/prototype_pattern.py
@@ -1,6 +1,7 @@
 # pola creational 
+# Penerapan Pola Desain "Prototype" (Pola Creational)
 
-import copy
+# Kelas dasar SpeedTest merupakan prototipe dasar yang mendefinisikan interface untuk kloning dan menjalankan tes.
 class SpeedTest:
     def clone(self):
         pass
@@ -8,9 +9,11 @@ class SpeedTest:
     def run_test(self):
         pass
 
+# Kelas-kelas berikut merupakan turunan dari SpeedTest yang mengimplementasikan metode kloning dan menjalankan tes.
+
 class KecepatanTest(SpeedTest):
     def clone(self):
-        return copy.copy(self)
+        return copy.copy(self)  # Mengkloning diri sendiri menggunakan copy.copy().
 
     def run_test(self):
         print("Mengukur kecepatan internet...")
@@ -36,14 +39,18 @@ class LatencyTest(SpeedTest):
     def run_test(self):
         print("Mengukur latency...")
 
-# penggunaan
+# Penggunaan Pola Desain "Prototype" di atas:
+
+# Membuat prototipe objek KecepatanTest sebagai objek dasar untuk kloning.
 speed_test_prototype = KecepatanTest()
 
+# Mengkloning prototipe objek KecepatanTest untuk membuat beberapa objek tes baru.
 test1 = speed_test_prototype.clone()
 test2 = speed_test_prototype.clone()
 test3 = speed_test_prototype.clone()
 test4 = speed_test_prototype.clone()
 
+# Menjalankan tes pada setiap objek yang telah di-klon.
 test1.run_test()
 test2.run_test()
 test3.run_test()
